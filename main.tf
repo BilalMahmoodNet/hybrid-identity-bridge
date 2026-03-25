d530f7538c31e8697a8add84da7fa630e15e5d24
Bilal Mahmood <mahmoodbilal50@googlemail.com>
2026-03-25 10:46:09 +0000

    feat: successful hybrid sync of PF Client and P1 Group

--- main.tf
+++ main.tf
@@ -13,3 +13,34 @@ output "hybrid_status_report" {
     local_pf_ver   = data.pingfederate_server_settings.product_version
   }
 }
+
+
+
+resource "pingfederate_oauth_client" "cc_secret_client" {
+  client_id = "cc_secret_client"
+  name      = "Client Credential Group"
+  enabled   = true
+
+  client_auth = {
+    type   = "SECRET"
+    secret = var.client_credentials_client_secret
+  }
+
+  grant_types = ["CLIENT_CREDENTIALS"]
+}
+
+resource "pingone_group" "cc_group" {
+  environment_id = var.pingone_environment_id 
+
+  name        = pingfederate_oauth_client.cc_secret_client.name
+  description = "My new awesome group for people who are awesome"
+
+  custom_data = jsonencode({
+    "client_id" = pingfederate_oauth_client.cc_secret_client.client_id
+  })
+
+  lifecycle {
+    # change the `prevent_destroy` parameter value to `true` to prevent this data carrying resource from being destroyed
+    prevent_destroy = false
+  }
+}
--- variable.tf
+++ variable.tf
@@ -2,3 +2,9 @@ variable "pingone_environment_id" {
     description = "PingOne Environment ID to connect to"
     type        = string
 }
+
+variable "client_credentials_client_secret" {
+    description = "Client Secret for the Client Credentials OAuth Client"
+    type        = string
+    sensitive   = true
+}  
