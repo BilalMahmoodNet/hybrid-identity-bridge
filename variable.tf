902445b63a07ecf7c91e773015bae9b3ffb84b99
Bilal Mahmood <mahmoodbilal50@googlemail.com>
2026-03-25 17:04:51 +0000

    added env in yml

--- variable.tf
+++ variable.tf
@@ -1,6 +1,27 @@
-variable "pingone_client_id" { type = string }
-variable "pingone_client_secret" { type = string; sensitive = true }
-variable "pingone_environment_id" { type = string }
-variable "pingfederate_admin_password" { type = string; sensitive = true }
-variable "client_credentials_client_secret" { type = string; sensitive = true }
-variable "pf_base_url" { type = string; default = "https://localhost:9999" }
+variable "pingone_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "pingone_client_id" {
+  type = string
+}
+
+variable "pingone_environment_id" {
+  type = string
+}
+
+variable "pingfederate_admin_password" {
+  type      = string
+  sensitive = true
+}
+
+variable "client_credentials_client_secret" {
+  type      = string
+  sensitive = true
+}
+
+variable "pf_base_url" {
+  type    = string
+  default = "https://localhost:9999"
+}
