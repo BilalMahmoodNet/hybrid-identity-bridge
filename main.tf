a1cefa5dd17cf69afc41f52ca777368c5c1d4c09
Bilal Mahmood <mahmoodbilal50@googlemail.com>
2026-03-25 18:25:54 +0000

    removed attempt to connect to local pingfederate

--- main.tf
+++ main.tf
@@ -3,14 +3,14 @@ data "pingone_environment" "my_env" {
   environment_id = var.pingone_environment_id # Define this var in variables.tf
 }
 
-# Data source to verify PingFederate connection
- data "pingfederate_server_settings" "product_version" {}
+# Data source to verify PingFederate connection - removed to prevent failing github actions workflow due to locl PF connection issues
+ #data "pingfederate_server_settings" "product_version" {}
 
 
 output "hybrid_status_report" {
   value = {
     cloud_env_name = data.pingone_environment.my_env.name
-    local_pf_ver   = data.pingfederate_server_settings.product_version
+   # local_pf_ver   = data.pingfederate_server_settings.product_version
   }
 }
 
